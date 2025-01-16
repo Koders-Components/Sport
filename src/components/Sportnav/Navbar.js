295368b786bb8512a797d669bcc6f917ee4ae907
Harshil0009 <harshilkheni200@gmail.com>
2025-01-16 17:22:23 +0530

    new change on old file

--- src/components/Sportnav/Navbar.js
+++ src/components/Sportnav/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { FaLocationDot } from "react-icons/fa6";
 import { Link, useNavigate } from 'react-router-dom';
@@ -10,19 +10,28 @@ export default function Header() {
     navigate('/Login'); // Redirects to the Login page
   };
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+
   return (
     <>
       <div className='containermain'>
         <div className='containerhead'>
           <div className='logoname'>
-            <div className='logo'>
+            <div className='logo sportnav'>
               <FaLocationDot />
             </div>
             <div className='lname'>
               <p>SPORTASTER</p>
             </div>
           </div>
-          <div className='menubtn'>
+          <button className="menu_toggle" onClick={toggleMenu}>
+            &#9776; {/* Three horizontal lines (hamburger icon) */}
+          </button>
+          <div className={`menubtn ${isMenuOpen ? 'open' : ''}`}>
             <div className='mmenu'>
               <div className='menu'><Link className='head' to='/user'>Home</Link></div>
               <div className='menu'><Link className='head' to='/user/Truf'>Turf</Link></div>
@@ -33,9 +42,9 @@ export default function Header() {
               <div className='menu'><Link className='head' to='/user/Collab'>Collab</Link></div>
               <div className='menu'><Link className='head' to='/user/Design'>Design</Link></div>
               <div className='menu'><Link className='head' to='/user/Invoice'>Invoice</Link></div>
-            </div>
-            <div className='btn'>
-              <button type='button' onClick={handleBookNow}>Login</button>
+              <div className='btn'>
+                <button type='button' onClick={handleBookNow}>Login</button>
+              </div>
             </div>
           </div>
         </div>
